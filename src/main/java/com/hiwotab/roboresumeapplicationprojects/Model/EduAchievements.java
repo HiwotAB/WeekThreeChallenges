1f0bc052ac97bff7a2e1ab5c00561bbbae110706
student <behabtuhiwot@gmail.com>
2017-08-14 11:35:53 -0400

    Second Version

--- src/main/java/com/hiwotab/roboresumeapplicationprojects/Model/EduAchievements.java
+++ src/main/java/com/hiwotab/roboresumeapplicationprojects/Model/EduAchievements.java
@@ -1,14 +1,10 @@
 package com.hiwotab.roboresumeapplicationprojects.Model;
 
 import org.hibernate.validator.constraints.NotEmpty;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -26,13 +22,9 @@ public class EduAchievements {
     @NotEmpty
     @Size(min=4)
     private String uniName;
-    @NotNull
-    @NotEmpty
-    @Min(1990)
-    @Max(2017)
+
     private int grdYear;
-    public String getEduType() {
-        return eduType;   }
+    public String getEduType() {return eduType;   }
 
     public void setEduType(String eduType) {
         this.eduType = eduType;
